221ca1e018c73ca004470c02cf32e2aea97a57f0
whw880218@pgtalk.com <whw880218@pgtalk.com>
2023-07-28 18:31:00 +0800

    新增onMounted,onUpdated

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -1,16 +1,31 @@
-import { defineComponent, ref, render } from "vue";
+import {
+  defineComponent,
+  ref,
+  render,
+  onMounted,
+  onUpdated,
+  onBeforeUnmount,
+} from "vue";
 import { ITask } from "../types/Task";
 import TaskItem from "./TaskItem.vue";
 
 export default defineComponent({
   setup() {
     const taskInput = ref("");
+    onMounted(() => {
+      // console.log("組件掛載到頁面之後執行");
+      console.log(taskInput.value);
+    });
+    onUpdated(() => {
+      // console.log("組件更新後執行");
+      console.log(taskInput.value);
+    });
     return {
       taskInput,
     };
   },
   render() {
-    return  (
+    return (
       <>
         <div class="flex justify-center item-center space-x-4">
           <input type="text" v-model={this.taskInput} />

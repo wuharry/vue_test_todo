ed7965fb00d751f16b91ea5a8877bd0eaf8184a5
whw880218@pgtalk.com <whw880218@pgtalk.com>
2023-07-28 18:21:26 +0800

    TaskList新增v-model,render函式

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -1,14 +1,19 @@
-import { defineComponent, ref } from "vue";
+import { defineComponent, ref, render } from "vue";
 import { ITask } from "../types/Task";
 import TaskItem from "./TaskItem.vue";
 
 export default defineComponent({
   setup() {
-    const count = ref(0);
-    return () => (
+    const taskInput = ref("");
+    return {
+      taskInput,
+    };
+  },
+  render() {
+    return  (
       <>
-        <div class="justify-center">
-          <input />
+        <div class="flex justify-center item-center space-x-4">
+          <input type="text" v-model={this.taskInput} />
           <button class="rounded bg-orange-300">Create Task</button>
         </div>
       </>

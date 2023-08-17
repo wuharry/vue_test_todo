e99d7a967c56adf543b144352d091b51c3a3bb78
whw880218@pgtalk.com <whw880218@pgtalk.com>
2023-08-17 17:18:59 +0800

    feat:taskItem beautify

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,16 @@
 import { createApp } from "vue";
 import "./style.css";
 import router from "./router";
-import store from './store'
+import store from "./store";
 import App from "./App.vue";
 import { OhVueIcon, addIcons } from "oh-vue-icons";
-import { IoClose } from "oh-vue-icons/icons";
+import { IoClose, RiDeleteBin2Line, FaEdit } from "oh-vue-icons/icons";
 
-addIcons(IoClose);
+addIcons(IoClose, RiDeleteBin2Line, FaEdit);
 // 創建一個 Vue 應用實例。它接受一個根組件作為參數,App
 const app = createApp(App)
-.use(router)
-.use(store)
-.component('VIcon', OhVueIcon)
-//將組件掛載到index.html中id=app的地方
-.mount("#app");
+  .use(router)
+  .use(store)
+  .component("VIcon", OhVueIcon)
+  //將組件掛載到index.html中id=app的地方
+  .mount("#app");

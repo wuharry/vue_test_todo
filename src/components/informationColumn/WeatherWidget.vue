ad54be491b9472f925c83646a340a731ad44b1df
wuharry <89292243+wuharry@users.noreply.github.com>
2023-08-20 23:14:03 +0800

    feat:add weather form

--- src/components/informationColumn/WeatherWidget.vue
+++ src/components/informationColumn/WeatherWidget.vue
@@ -12,7 +12,6 @@ const fetchWeatherData = async () => {
     console.log(response.data);
   } catch (error) {
     console.log(`失敗`);
-
     console.error(error);
   }
 };
@@ -24,15 +23,26 @@ const count = ref(0);
 
 <template>
   <div>
-    <p>Weather Widget Content Here</p>
+    <!-- <p>Weather Widget Content Here</p> -->
+    <div class="weatherCard">
+      <!-- 降雨機率,風速,濕度 -->
+      <div></div>
+      <!-- 溫度,溫度range -->
+      <div></div>
+      <!-- 位置,時間(根據時間,天氣背景也要更換) -->
+      <div></div>
+      <!-- 氣候圖片 -->
+      <div></div>
+    </div>
   </div>
 </template>
 
 <style scoped lang="scss">
 .weatherCard {
-  width: 11em;
-  height: 13em;
-  background-color: black;
+  width: 15em;
+  height: 17em;
+  margin-top: 1em;
+  background-color: rgba(29, 126, 222, 0.388);
   border-radius: 1em;
 }
 </style>

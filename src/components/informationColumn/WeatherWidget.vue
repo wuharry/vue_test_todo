d425b12a4876bf5ca2f3ba250120f38c0b7ed3d2
whw880218@pgtalk.com <whw880218@pgtalk.com>
2023-08-21 18:19:57 +0800

    test:調整weatherCard CSS

--- src/components/informationColumn/WeatherWidget.vue
+++ src/components/informationColumn/WeatherWidget.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { ref, onMounted } from "vue";
+import { ref, onMounted, watch } from "vue";
 import axios from "axios";
 const weatherData = ref({
   location: '',
@@ -8,6 +8,7 @@ const weatherData = ref({
   uv: 0,
   windKph: 0,
   pm2_5: 0,
+  weather: ''
 });
 const fetchWeatherData = async () => {
   const options = {
@@ -23,6 +24,7 @@ const fetchWeatherData = async () => {
     weatherData.value.uv = response.data.current.uv;
     weatherData.value.windKph = response.data.current.wind_kph;
     weatherData.value.pm2_5 = response.data.current.air_quality.pm2_5;
+    weatherData.value.weather = response.data.current.condition.text;
     console.log(weatherData.value);
   } catch (error) {
     console.log(`失敗`);
@@ -32,53 +34,390 @@ const fetchWeatherData = async () => {
 
 fetchWeatherData();
 
-const count = ref(0);
+const weatherCardRef = ref();
+weatherData.value.weather = 'sun';
+watch(() => weatherData.value.weather, (newValue, oldValue) => {
+  console.log(newValue);
+  weatherCardRef.value.classList.add('hot');
+});
+
 </script>
 
 <template>
   <div>
     <!-- <p>Weather Widget Content Here</p> -->
-    <div class="weatherCard">
-      <!-- 溫度,風速,濕度 -->
-      <div class="cardTop">
-        <!-- 降雨機率暫不提供改為溫度 -->
-        <div>
-          <VIcon class="" name="la-temperature-high-solid" />
-          <span>{{ weatherData.tempC }}</span>
-        </div>
-        <div>
-          <VIcon class="" name="ri-windy-line" />
-          <span> {{ weatherData.windKph }}</span>
-        </div>
-        <div>
-          <VIcon class="" name="wi-humidity" />
-          <span>{{ weatherData.humidity }}</span>
-        </div>
+    <div ref="weatherCardRef">
+      <span class="sun"></span>
+      <span class="sunx"></span>
+      <div class="weatherCard">
+        <!-- 溫度,風速,濕度 -->
+        <div class="cardTop">
+          <!-- 降雨機率暫不提供改為溫度 -->
+          <div>
+            <VIcon class="" name="la-temperature-high-solid" />
+            <span>{{ weatherData.tempC }}</span>
+          </div>
+          <div>
+            <VIcon class="" name="ri-windy-line" />
+            <span> {{ weatherData.windKph }}</span>
+          </div>
+          <div>
+            <VIcon class="" name="wi-humidity" />
+            <span>{{ weatherData.humidity }}</span>
+          </div>
 
-      </div>
-      <!-- pm2.5,uv,天氣 -->
-      <div class="">
+        </div>
+        <!-- pm2.5,uv,天氣 -->
+        <div class="cardMid">
+          <!-- <div>
+          <VIcon class="" name="" />
+          <span>{{ }}</span>
+        </div> -->
+          <div>
+            <VIcon class="" name="" />
+            <span>{{ }}</span>
+          </div>
+          <!-- <div>
+          <VIcon class="" name="" />
+          <span>{{ }}</span>
+        </div> -->
+          <div>
+            <VIcon class="" name="" />
+            <span>{{ }}</span>
+          </div>
+        </div>
+        <!-- 位置,時間(根據時間,天氣背景也要更換) -->
+        <div>
 
+        </div>
+        <!-- 氣候圖片 -->
+        <div></div>
       </div>
-      <!-- 位置,時間(根據時間,天氣背景也要更換) -->
-      <div></div>
-      <!-- 氣候圖片 -->
-      <div></div>
     </div>
   </div>
 </template>
 
 <style scoped lang="scss">
 .weatherCard {
-  width: 15em;
-  height: 17em;
+  width: 11em;
+  height: 16em;
   margin-top: 1em;
-  background-color: rgba(29, 126, 222, 0.388);
+  // background-color: rgba(29, 126, 222, 0.388);
   border-radius: 1em;
 }
 
 .cardTop {
   display: flex;
   justify-content: space-around;
+}
+
+.cardMid {
+  display: flex;
+}
+
+// 晴天+晴天背景加設置
+.hot {
+  // position: absolute;
+  // top: 25%;
+  // left: 20%;
+  background: linear-gradient(to top right,
+      rgba(255, 87, 34, 1) 0%,
+      rgba(251, 140, 0, 1) 100%);
+  box-shadow: 1px 1px 30px rgba(255, 111, 0, 1);
+  border-radius: 1em;
+}
+
+.sun {
+  position: absolute;
+  top: 3%;
+  left: 93%;
+  width: 50px;
+  height: 50px;
+  border-radius: 100%;
+  background: linear-gradient(to bottom left,
+      rgba(255, 235, 59, 1) 0%,
+      rgba(249, 168, 37, 1) 90%);
+  box-shadow: 1px 1px 30px rgba(255, 160, 0, 1);
+  animation: inex 3s infinite linear;
+}
+
+.sunx {
+  position: absolute;
+  top: 30%;
+  left: 45%;
+  width: 10px;
+  height: 10px;
+  border-radius: 100%;
+  background-color: #fff;
+  opacity: 0.2;
+}
+
+// 陰天
+.cloudy {
+  // position: absolute;
+  // top: 25%;
+  // left: 45%;
+  background: linear-gradient(to top right,
+      rgba(63, 81, 181, 1) 0%,
+      rgba(3, 155, 229, 1) 40%);
+  box-shadow: 1px 1px 30px rgba(2, 119, 189, 1);
+  border-radius: 1em;
+}
+
+.cloud {
+  position: absolute;
+  top: 5%;
+  left: 70%;
+  width: 60px;
+  height: 20px;
+  border-radius: 10px;
+  background-color: rgba(129, 212, 250, 1);
+  box-shadow: 1px 1px 30px rgba(0, 151, 167, 1);
+  animation: move 3s infinite linear;
+}
+
+.cloudx {
+  position: absolute;
+  top: 23%;
+  left: 55%;
+  width: 60px;
+  height: 20px;
+  border-radius: 10px;
+  background-color: rgba(129, 212, 250, 1);
+  animation: move 3s infinite linear;
+}
+
+// 下雪
+.stormy {
+  position: absolute;
+  top: 25%;
+  left: 70%;
+  background: linear-gradient(to top right,
+      rgba(117, 117, 117, 1) 0%,
+      rgba(33, 33, 33, 1) 90%);
+  box-shadow: 1px 1px 30px rgba(33, 33, 33, 1);
+  border-radius: 1em;
+}
+
+.snowe {
+  position: absolute;
+  top: 60%;
+  left: 40%;
+  width: 40px;
+  height: 40px;
+  border-radius: 60px;
+  background-color: #ddd;
+}
+
+.snowex {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  width: 15px;
+  height: 15px;
+  border-radius: 15px;
+  background-color: #bbb;
+}
+
+.stick {
+  position: absolute;
+  top: 60%;
+  left: 40%;
+  width: 3px;
+  height: 15px;
+  transform: rotate(-45deg);
+  background-color: #333;
+  z-index: -10;
+}
+
+.stick2 {
+  position: absolute;
+  top: 60%;
+  left: 70%;
+  width: 3px;
+  height: 15px;
+  transform: rotate(45deg);
+  background-color: #333;
+  z-index: -10;
+}
+
+// 閃電
+.breezy {
+  // position: absolute;
+  top: 60%;
+  left: 30%;
+  background: linear-gradient(to top right,
+      rgba(156, 204, 101, 1) 0%,
+      rgba(38, 198, 218, 1) 50%);
+  box-shadow: 1px 1px 30px rgba(38, 198, 218, 1);
+  border-radius: 1em;
+}
+
+.cloudr {
+  position: absolute;
+  top: 5%;
+  left: 60%;
+  width: 60px;
+  height: 20px;
+  border-radius: 10px;
+  background-color: rgba(96, 125, 139, 1);
+  box-shadow: 1px 1px 30px rgba(84, 110, 122, 1);
+  animation: flash 1.5s infinite linear;
+}
+
+// 晚上
+.night {
+  // position: absolute;
+  top: 60%;
+  left: 60%;
+  background: linear-gradient(to bottom right,
+      rgba(63, 81, 181, 1) 0%,
+      rgba(171, 71, 188, 1) 70%);
+  box-shadow: 1px 1px 30px rgba(81, 45, 168, 1);
+  border-radius: 1em;
+}
+
+.moon {
+  position: absolute;
+  top: -10%;
+  left: 65%;
+  width: 70px;
+  height: 70px;
+  border-radius: 100%;
+  background: rgba(255, 241, 118, 1);
+  box-shadow: 1px 1px 30px rgba(224, 224, 224, 1);
+}
+
+.spot1 {
+  position: absolute;
+  top: 0%;
+  left: 85%;
+  width: 10px;
+  height: 10px;
+  border-radius: 100%;
+  background-color: #777;
+}
+
+.spot2 {
+  position: absolute;
+  top: 30%;
+  left: 75%;
+  width: 5px;
+  height: 5px;
+  border-radius: 100%;
+  background-color: #777;
+}
+
+.night li {
+  position: absolute;
+  list-style: none;
+  width: 3px;
+  height: 3px;
+  border-radius: 100%;
+  background-color: #fff;
+
+  transform: rotate(45deg);
+}
+
+.night li:nth-child(1) {
+  top: 30%;
+  left: 65%;
+}
+
+.night li:nth-child(2) {
+  top: 35%;
+  left: 53%;
+}
+
+.night li:nth-child(3) {
+  opacity: 0;
+  top: 20%;
+  left: 75%;
+  width: 2px;
+  height: 2px;
+  animation: meteor 1.5s infinite linear;
+  animation-delay: 0.6s;
+}
+
+.night li:nth-child(4) {
+  top: 5%;
+  left: 50%;
+}
+
+.night li:nth-child(5) {
+  opacity: 0;
+  top: 20%;
+  left: 55%;
+  width: 1px;
+  height: 15px;
+  animation: meteor 1.5s infinite linear;
+}
+
+
+
+
+
+
+
+// 動畫格式宣告
+@keyframes inex {
+  50% {
+    opacity: 0.4;
+  }
+
+  60% {
+    opacity: 1;
+  }
+}
+
+@keyframes move {
+  50% {
+    transform: translateX(-10px);
+  }
+}
+
+@keyframes fall {
+  10% {
+    opacity: 0.8;
+  }
+
+  50% {
+    opacity: 1;
+    transform: translate(-10px, 30px);
+  }
+
+  100% {
+    transform: translate(-25px, 70px);
+  }
+}
+
+@keyframes flash {
+  48% {
+    background-color: rgba(96, 125, 139, 1);
+  }
+
+  50% {
+    background-color: #fff;
+  }
+
+  55% {
+    background-color: rgba(96, 125, 139, 1);
+  }
+
+  57% {
+    background-color: #fff;
+  }
+}
+
+@keyframes meteor {
+  10% {
+    opacity: 1;
+  }
+
+  80% {
+    left: 10%;
+    top: 75%;
+    opacity: 0;
+  }
 }
 </style>

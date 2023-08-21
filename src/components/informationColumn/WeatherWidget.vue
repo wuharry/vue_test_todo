17ac53c4d9731a202b0c3870b90f9737ffd18cbc
whw880218@pgtalk.com <whw880218@pgtalk.com>
2023-08-21 16:54:18 +0800

    feat:add weatherData into template

--- src/components/informationColumn/WeatherWidget.vue
+++ src/components/informationColumn/WeatherWidget.vue
@@ -1,6 +1,14 @@
 <script setup lang="ts">
 import { ref, onMounted } from "vue";
 import axios from "axios";
+const weatherData = ref({
+  location: '',
+  tempC: 0,
+  humidity: 0,
+  uv: 0,
+  windKph: 0,
+  pm2_5: 0,
+});
 const fetchWeatherData = async () => {
   const options = {
     method: "GET",
@@ -8,8 +16,14 @@ const fetchWeatherData = async () => {
   };
   try {
     const response = await axios.request(options);
-    console.log(`天氣`);
     console.log(response.data);
+    weatherData.value.location = response.data.location.name;
+    weatherData.value.tempC = response.data.current.temp_c;
+    weatherData.value.humidity = response.data.current.humidity;
+    weatherData.value.uv = response.data.current.uv;
+    weatherData.value.windKph = response.data.current.wind_kph;
+    weatherData.value.pm2_5 = response.data.current.air_quality.pm2_5;
+    console.log(weatherData.value);
   } catch (error) {
     console.log(`失敗`);
     console.error(error);
@@ -25,9 +39,14 @@ const count = ref(0);
   <div>
     <!-- <p>Weather Widget Content Here</p> -->
     <div class="weatherCard">
-      <!-- 降雨機率,風速,濕度 -->
-      <div></div>
-      <!-- 溫度,溫度range -->
+      <!-- 溫度,風速,濕度 -->
+      <div class="cardTop">
+        <!-- 降雨機率暫不提供改為溫度 -->
+        <span> {{ weatherData.tempC }}</span>
+        <span> {{ weatherData.windKph }}</span>
+        <span>{{ weatherData.humidity }}</span>
+      </div>
+      <!-- pm2.5,uv,天氣 -->
       <div></div>
       <!-- 位置,時間(根據時間,天氣背景也要更換) -->
       <div></div>
@@ -44,5 +63,10 @@ const count = ref(0);
   margin-top: 1em;
   background-color: rgba(29, 126, 222, 0.388);
   border-radius: 1em;
+}
+
+.cardTop {
+  display: flex;
+  justify-content: space-around;
 }
 </style>

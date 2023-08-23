570eef888989b2ac090f66e62a8f5553a424755a
whw880218@pgtalk.com <whw880218@pgtalk.com>
2023-08-23 18:34:22 +0800

    feat:add 上方控制區

--- src/components/informationColumn/Calendar.vue
+++ src/components/informationColumn/Calendar.vue
@@ -2,7 +2,20 @@
     <div class="calendar">
         <!-- 上方控制區 -->
         <div class="calendar-controls">
-            
+            <div class="calendar-prev"><a href="#"><svg xmlns="http://www.w3.org/2000/svg" width="128" height="128"
+                        viewBox="0 0 128 128">
+                        <path fill="#666" d="M88.2 3.8L35.8 56.23 28 64l7.8 7.78 52.4 52.4 9.78-7.76L45.58 64l52.4-52.4z" />
+                    </svg></a></div>
+            <div class="calendar-year-month">
+                <div class="calendar-month-label"></div>
+                <div>-</div>
+                <div class="calendar-year-label"></div>
+            </div>
+            <div class="calendar-next"><a href="#"><svg xmlns="http://www.w3.org/2000/svg" width="128" height="128"
+                        viewBox="0 0 128 128">
+                        <path fill="#666"
+                            d="M38.8 124.2l52.4-52.42L99 64l-7.77-7.78-52.4-52.4-9.8 7.77L81.44 64 29 116.42z" />
+                    </svg></a></div>
         </div>
         <!-- 中間區塊 -->
         <div class="calendar-today-date"></div>
@@ -227,5 +240,4 @@ onMounted(() => {
 
 .calendar.calendar-body .calendar-today a {
     outline: 2px solid var(--calendar-today-innerborder-color);
-}
-</style>
+}</style>

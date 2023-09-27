b7507c85a0403a3325dbdd74e2360ab6360f726b
wuharvey <whw880218@pgtalk.com>
2023-09-27 18:29:16 +0800

    test:2/17

---
+++ src/components/clender/Calendar_Month.vue
@@ -0,0 +1,150 @@
+<template>
+    <div class="calendar-body">
+        <!-- 星期顯示 -->
+        <div class="Week" v-for="day in calWeekDays">
+            {{ day }}
+        </div>
+        <!--前一個月多出的天數  -->
+        <div></div>
+        <!-- 月天數顯示 -->
+        <div v-for="day in currentMonthDays">
+            <div class="Day">
+                {{ day }}
+            </div>
+
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { ref, computed } from 'vue';
+const localDate = ref(new Date()); //當前時間'固定'
+const year = computed(() => localDate.value.getFullYear());
+const calMonthName = [
+    "Jan", "Feb", "Mar", "Apr", "May", "Jun",
+    "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"
+];
+const month = computed(() => calMonthName[localDate.value.getMonth()]);
+const today = computed(() => localDate.value.getDate());//當前日期
+const calWeekDays = ref(["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]);
+const currentMonthDays = ref<number[]>([]);//當前月天數
+const calendarControl = {
+    localDate: new Date(),
+    // 其他程式碼屬性和方法
+};
+
+// 當前月有幾天
+const daysInMonth = (year: number, month: number): number => {
+    return new Date(year, month, 0).getDate();
+}
+const firstDay = (): Date => {
+    // calendar.value.getMonth() 1月是0,12月是11
+    return new Date(localDate.value.getFullYear(), localDate.value.getMonth(), 1);
+}
+
+// 獲取當月第一天星期
+const firstDayNumber = (): number => {
+    return firstDay().getDay();
+}
+
+// 獲取當月最後一天星期
+const lastDay = (): Date => {
+    return new Date(localDate.value.getFullYear(), localDate.value.getMonth() + 1, 0);
+}
+const lastDayNumber = (): number => {
+    return lastDay().getDay();
+}
+
+// 獲取前一個月總天數
+const getPreviousMonthLastDate = (): number => {
+    let lastDate = new Date(
+        localDate.value.getFullYear(),
+        localDate.value.getMonth(),
+        0
+    ).getDate();
+    return lastDate;
+}
+// 導資料到前一個月
+const navigateToPreviousMonth = (): void => {
+    localDate.value = new Date(localDate.value.setMonth(localDate.value.getMonth() - 1));
+    setCalenderData();
+}
+// 導資料到後一個月
+const navigateToNextMonth = (): void => {
+    localDate.value = new Date(localDate.value.setMonth(localDate.value.getMonth() + 1));
+    setCalenderData();
+}
+// 導資料到當前月
+const navigateToCurrentMonth = (): void => {
+    let currentMonth = calendarControl.localDate.getMonth();
+    let currentYear = calendarControl.localDate.getFullYear();
+    localDate.value.setMonth(currentMonth);
+    localDate.value.setFullYear(currentYear);
+    setCalenderData();
+}
+let todayindex = ref(0);
+const setCalenderData = (): void => {
+    currentMonthDays.value = [];
+    const firstDayOfMonth = lastDayNumber()
+    const daysPerMonth = daysInMonth(year.value, (localDate.value.getMonth() + 1));
+    const daysPreMonth = getPreviousMonthLastDate();
+    console.log(daysPreMonth);
+    
+    for (let day = firstDayOfMonth; day > 0; day--) {
+        console.log(daysPreMonth + 1 - day);
+        currentMonthDays.value.push(daysPreMonth + 1 - day);
+    }
+    for (let day = 1; day <= daysPerMonth; day++) {
+        currentMonthDays.value.push(day);
+        if (day === today.value) {
+            todayindex.value = currentMonthDays.value.indexOf(day);
+        }
+
+    }
+}
+setCalenderData();
+</script>
+<style scoped lang='scss'>
+@mixin custom-border {
+    font-size: 1rem;
+    line-height: 1.5rem;
+    text-align: center;
+    box-sizing: border-box;
+    border-style: solid;
+    border-color: #c1c8cf;
+    border-width: 1px;
+}
+
+.calendar-body {
+    margin: 0;
+    padding: 0;
+    width: 100%;
+    height: 100vh;
+    display: grid;
+    grid-template-columns: repeat(7, minmax(0, 1fr));
+    grid-template-rows: 20px repeat(5, minmax(0, 1fr));
+
+}
+
+.Week {
+    @include custom-border;
+}
+
+.Day {
+    @include custom-border;
+    width: 100%;
+    height: 5.7em;
+}
+
+@keyframes slideIn {
+    0% {}
+
+    100% {
+        background-color: rgb(202, 182, 182);
+    }
+}
+
+.Day:hover {
+    animation: slideIn .45s 0s cubic-bezier(0.175, 0.885, 0.320, 1.275);
+}
+</style>
